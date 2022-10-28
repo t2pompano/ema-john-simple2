1617d8ca1bf80fc6868fc5f7c103da43a6f62537
t2pompano <t2pompano@gmail.com>
2022-10-28 10:04:16 -0400

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const { name, seller, price, img, ratings } = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings}</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p className='btn-text'>Add To Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
